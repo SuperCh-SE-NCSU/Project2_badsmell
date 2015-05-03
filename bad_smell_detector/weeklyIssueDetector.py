bce9ee478fea01c4cc5243d1794b3a3aadee4854
Winbobob <zhu6@ncsu.edu>
2015-05-03 15:27:34 -0400

    update bad smell detectors

--- bad_smell_detector/weeklyIssueDetector.py
+++ bad_smell_detector/weeklyIssueDetector.py
@@ -5,10 +5,11 @@ def weeklyIssueDetector():
     #weeklyIssue = gt.getIssueNumPerWeek()
     #print(weeklyIssue)
     #weeklyIssue=[24, 9, 4, 2, 5, 6, 5, 8]
-    weeklyIssue=[10, 12, 4, 1, 1, 1, 1, 37, 1]
-    #weeklyIssue=[21, 8, 9, 2, 9, 10, 26, 5]
+    #weeklyIssue=[10, 12, 4, 1, 1, 1, 1, 37, 1]
+    weeklyIssue=[21, 8, 9, 2, 9, 10, 26, 5]
     mean = np.mean(weeklyIssue)
     std_dev = np.std(weeklyIssue)
+    print('weeklyIssue:',weeklyIssue)
     print('mean:', mean)
     print('std_dev:', std_dev)
     for i in range(len(weeklyIssue)):
@@ -18,7 +19,7 @@ def weeklyIssueDetector():
         elif weeklyIssue[i] < mean-std_dev:
             print('Badsmell: Too little issues in this week.')
         else:
-            print('Issue number is normal in this week')
+            print('Issue number is normal in this week.')
 
 
 
